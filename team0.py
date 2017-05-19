d4479410232bdd3223a2315b2345c3024a512ff3
diercksy81 <adiercks81@gmail.com>
2017-05-19 12:29:02 -0500

    Jake And Alex Team 0
    Jake Lea and Alex Diercks Team 0

--- team0.py
+++ team0.py
@@ -1,4 +1,4 @@
-####
+import random
 # Each team's file must define four tokens:
 #     team_name: a string
 #     strategy_name: a string
@@ -6,63 +6,76 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'EngageFizzyMode'
+strategy_name = 'Top 3'
+strategy_description = "Wingin' it"
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
+    '''Make my move based on the history with this player.
     
-    Make my move.
-    Returns 'c' or 'b'. 
+    history: a string with one letter (c or b) per round that has been played with this opponent.
+    their_history: a string of the same length as history, possibly empty. 
+    The first round between these two players is my_history[0] and their_history[0]
+    The most recent round is my_history[-1] and their_history[-1]
+    
+    Returns 'c' or 'b' for collude or betray.
     '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
-
-    
-def test_move(my_history, their_history, my_score, their_score, result):
-    '''calls move(my_history, their_history, my_score, their_score)
-    from this module. Prints error if return value != result.
-    Returns True or False, dpending on whether result was as expected.
-    '''
-    real_result = move(my_history, their_history, my_score, their_score)
-    if real_result == result:
-        return True
-    else:
-        print("move(" +
-            ", ".join(["'"+my_history+"'", "'"+their_history+"'",
-                       str(my_score), str(their_score)])+
-            ") returned " + "'" + real_result + "'" +
-            " and should have returned '" + result + "'")
-        return False
-
-if __name__ == '__main__':
-     
-    # Test 1: Betray on first move.
-    if test_move(my_history='',
-              their_history='', 
-              my_score=0,
-              their_score=0,
-              result='b'):
-         print 'Test passed'
-     # Test 2: Continue betraying if they collude despite being betrayed.
-    test_move(my_history='bbb',
-              their_history='ccc', 
-              # Note the scores are for testing move().
-              # The history and scores don't need to match unless
-              # that is relevant to the test of move(). Here,
-              # the simulation (if working correctly) would have awarded 
-              # 300 to me and -750 to them. This test will pass if and only if
-              # move('bbb', 'ccc', 0, 0) returns 'b'.
-              my_score=0, 
-              their_score=0,
-              result='b')             
+    while len(my_history)<9:
+        if len(my_history)==0:
+            return 'c'
+        elif len(my_history)==1:
+            return 'c'
+        elif len(my_history)==2:
+            return 'b'
+        elif len(my_history)==3:
+            return 'c'
+        elif len(my_history)==4:
+            return 'b'
+        elif len(my_history)==5:
+            return 'b'
+        elif len(my_history)==6:
+            return 'c'
+        elif len(my_history)==7:
+            return 'b'
+        elif len(my_history)==8:
+            return 'c'
+        elif len(my_history)==9:
+            return 'b'
+    while len(my_history)>9:
+        c = their_history.count('b')
+        t = float(c)/len(their_history)
+        n = my_history[-1]
+        m = their_history[-1]
+        recent_round_them = their_history[-1]
+        recent_round_me = my_history[-1]
+        if (their_history[-5:] == 'cbcbc') or (their_history[-5:] == 'bcbcb'):
+            return 'b'
+        elif (their_history[-5:] == 'bbbbb'):
+            return 'b'
+        elif (their_history[-5:] == 'ccccc'):
+            return 'c'
+        elif (their_history[-5:].count('b'))>2:
+            return 'b'
+        
+        elif random.random()<.5:
+            for round in range(len(my_history)-1):
+                prior_round_them = their_history[round]
+                prior_round_me = my_history[round]
+                if (prior_round_me == recent_round_me) and \
+                    (prior_round_them == recent_round_them):
+                    return their_history[round]
+        elif random.random()<t:
+            return 'b'
+        elif n == 'b' and m == 'c':
+            if random.random()<.65:
+                return 'c'
+        elif n == 'c' and m == 'c':
+            if random.random()<.70:
+                return 'c'
+        elif (n == 'b' and m == 'b') or (n == 'c' and m == 'b'):
+            if random.random()<.60:
+                return 'b'
+        else:
+            return 'c'
+        #else:
+        #    return 'c'
